5c2b641f8411459e692d94477e5908d37f6f8906
Kazuki Yunoue <you.know.ue1480@gmail.com>
2012-09-01 00:13:06 +0900

    del unnesessary file

--- Gemfile
+++ Gemfile
@@ -11,7 +11,6 @@ end
 
 group :production do
   gem 'pg'
-  gem 'thin'
 end
 
 # Gems used only for assets and not required
--- app/models/message.rb
+++
@@ -1,5 +0,0 @@
-class Message < ActiveResource::Base
-  self.site = "https://www.yammer.com"
-  self.prefix = "/api/v1/"
-  self.format = :json
-end
